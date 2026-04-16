1e291961056178fb860c98045e3349d78e788181
Meikou <121649289+Meikouz@users.noreply.github.com>
2026-04-16 17:16:30 +0300

    Add files via upload

---
+++ ЕкзаменКПЗ/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ЕкзаменКПЗ
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnTxt;
+        private System.Windows.Forms.Button btnHtml;
+        private System.Windows.Forms.TextBox txtFinance;
+        private System.Windows.Forms.TextBox txtSales;
+        private System.Windows.Forms.Label lblFinance;
+        private System.Windows.Forms.Label lblSales;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtFinance = new System.Windows.Forms.TextBox();
+            this.txtSales = new System.Windows.Forms.TextBox();
+            this.btnTxt = new System.Windows.Forms.Button();
+            this.btnHtml = new System.Windows.Forms.Button();
+            this.lblFinance = new System.Windows.Forms.Label();
+            this.lblSales = new System.Windows.Forms.Label();
+
+            // Label Finance
+            this.lblFinance.Text = "Фінанси:";
+            this.lblFinance.Location = new System.Drawing.Point(50, 20);
+
+            // txtFinance
+            this.txtFinance.Location = new System.Drawing.Point(50, 40);
+            this.txtFinance.Size = new System.Drawing.Size(300, 22);
+
+            // Label Sales
+            this.lblSales.Text = "Продажі:";
+            this.lblSales.Location = new System.Drawing.Point(50, 70);
+
+            // txtSales
+            this.txtSales.Location = new System.Drawing.Point(50, 90);
+            this.txtSales.Size = new System.Drawing.Size(300, 22);
+
+            // btnTxt
+            this.btnTxt.Location = new System.Drawing.Point(50, 130);
+            this.btnTxt.Size = new System.Drawing.Size(130, 30);
+            this.btnTxt.Text = "TXT";
+            this.btnTxt.Click += new System.EventHandler(this.btnTxt_Click);
+
+            // btnHtml
+            this.btnHtml.Location = new System.Drawing.Point(220, 130);
+            this.btnHtml.Size = new System.Drawing.Size(130, 30);
+            this.btnHtml.Text = "HTML";
+            this.btnHtml.Click += new System.EventHandler(this.btnHtml_Click);
+
+            // Form
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.lblFinance);
+            this.Controls.Add(this.txtFinance);
+            this.Controls.Add(this.lblSales);
+            this.Controls.Add(this.txtSales);
+            this.Controls.Add(this.btnTxt);
+            this.Controls.Add(this.btnHtml);
+            this.Text = "Visitor Pattern - Reports";
+        }
+    }
+}
---
+++ ЕкзаменКПЗ/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ЕкзаменКПЗ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        interface IReport
+        {
+            void Accept(IReportVisitor visitor);
+        }
+
+        interface IReportVisitor
+        {
+            void Visit(FinancialReport report);
+            void Visit(SalesReport report);
+        }
+
+        class FinancialReport : IReport
+        {
+            public string Data { get; set; }
+
+            public FinancialReport(string data)
+            {
+                Data = data;
+            }
+
+            public void Accept(IReportVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+
+        class SalesReport : IReport
+        {
+            public string Data { get; set; }
+
+            public SalesReport(string data)
+            {
+                Data = data;
+            }
+
+            public void Accept(IReportVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+
+        class TxtExportVisitor : IReportVisitor
+        {
+            private string filePath;
+
+            public TxtExportVisitor(string path)
+            {
+                filePath = path;
+                File.WriteAllText(filePath, "");
+            }
+
+            public void Visit(FinancialReport report)
+            {
+                File.AppendAllText(filePath, "Фінанси: " + report.Data + "\n");
+            }
+
+            public void Visit(SalesReport report)
+            {
+                File.AppendAllText(filePath, "Продажі: " + report.Data + "\n");
+            }
+        }
+
+        class HtmlExportVisitor : IReportVisitor
+        {
+            private string filePath;
+
+            public HtmlExportVisitor(string path)
+            {
+                filePath = path;
+                File.WriteAllText(filePath, "<html><body>\n");
+            }
+
+            public void Visit(FinancialReport report)
+            {
+                File.AppendAllText(filePath,
+                    $"<h2>Фінанси</h2><p>{report.Data}</p>\n");
+            }
+
+            public void Visit(SalesReport report)
+            {
+                File.AppendAllText(filePath,
+                    $"<h2>Продажі</h2><p>{report.Data}</p>\n");
+            }
+
+            public void Close()
+            {
+                File.AppendAllText(filePath, "</body></html>");
+            }
+        }
+
+        private void btnTxt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFinance.Text) ||
+                string.IsNullOrWhiteSpace(txtSales.Text))
+            {
+                MessageBox.Show("Заповніть обидва поля!");
+                return;
+            }
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "TXT (*.txt)|*.txt";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var reports = new IReport[]
+                {
+                    new FinancialReport(txtFinance.Text),
+                    new SalesReport(txtSales.Text)
+                };
+
+                var visitor = new TxtExportVisitor(dialog.FileName);
+
+                foreach (var r in reports)
+                    r.Accept(visitor);
+
+                MessageBox.Show("TXT створено!");
+            }
+        }
+
+        private void btnHtml_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFinance.Text) ||
+                string.IsNullOrWhiteSpace(txtSales.Text))
+            {
+                MessageBox.Show("Заповніть обидва поля!");
+                return;
+            }
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "HTML (*.html)|*.html";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var reports = new IReport[]
+                {
+                    new FinancialReport(txtFinance.Text),
+                    new SalesReport(txtSales.Text)
+                };
+
+                var visitor = new HtmlExportVisitor(dialog.FileName);
+
+                foreach (var r in reports)
+                    r.Accept(visitor);
+
+                visitor.Close();
+
+                MessageBox.Show("HTML створено!");
+            }
+        }
+    }
+}
